c894c189a00fc1789662cb2746a271e9d2e12bf4
antonfridlund <31419272+AntonFridlund@users.noreply.github.com>
2024-11-16 00:28:46 +0100

    Add graceful shutdown handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,82 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"main/middlewares/log"
 	"main/routes"
 	"net/http"
 	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
+	host := "localhost"
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	mainRouter := routes.NewRouter()
-	sizeLimit := http.MaxBytesHandler(mainRouter, 1*1024*1024)
-	logger := log.LogMiddleware(sizeLimit)
+	// Server settings
+	server := http.Server{
+		Addr:              host + ":" + port,
+		MaxHeaderBytes:    2 * 1024 * 1024,
+		ReadHeaderTimeout: 3 * time.Second,
+		ReadTimeout:       4 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		Handler:           routes.NewRouter(),
+	}
+	server.Handler = log.LogMiddleware(server.Handler)
 
-	// Write log channel entries to stdout
+	// Goroutine to process log entries
 	go func() {
 		for logEntry := range log.LogChannel {
 			os.Stdout.Write([]byte(logEntry))
 		}
 	}()
 
-	fmt.Println("Server has started at http://localhost:" + port)
-	if err := http.ListenAndServe(":"+port, logger); err != nil {
-		fmt.Println("Error starting server:", err)
+	// Start server in separate goroutine
+	go func() {
+		fmt.Println("Server started successfully")
+		msg := fmt.Sprintf("Listening to: %s:%s", host, port)
+		fmt.Printf("%s\n%s\n", msg, strings.Repeat("-", len(msg)))
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Println("Error starting server:", err)
+		}
+	}()
+
+	// Handle graceful shutdown logic
+	var graceWaitGroup sync.WaitGroup
+	graceWaitGroup.Add(1)
+	server.RegisterOnShutdown(func() {
+		fmt.Println("Initiating graceful shutdown")
+		defer graceWaitGroup.Done()
+		close(log.LogChannel)
+	})
+
+	// Wait for shutdown signal
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	fmt.Println("Received shutdown signal")
+
+	// Set timeout for graceful shutdown
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	// Gracefully shut down the server
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Println("Error during shutdown:", err)
 	}
+
+	// Wait for shutdown function to complete
+	graceWaitGroup.Wait()
+	fmt.Println("Server gracefully stopped")
 }
